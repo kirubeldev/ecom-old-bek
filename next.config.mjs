c39c6fc2ecf53ab7be2f38f3f3c3fbe53a7f3690
=Kirubel Meneberu <=kirubel.menberu@kegeberewtech.com>
2024-08-07 02:03:04 +0300

    ms config changed

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {images: {
+const nextConfig = {
+  images: {
     domains: ['images.unsplash.com'],
-  },};
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
-export default nextConfig;
+module.exports = nextConfig;
